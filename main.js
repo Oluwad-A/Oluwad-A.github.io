9424ab73dda66547388aaa0e39a0f6584d4aafb9
Oluwad-A <akinsolaoluwadamilare64@gmail.com>
2026-02-04 23:23:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -229,7 +229,6 @@ function changePDFPage(pdfId, pdfMaxPage, direction) {
         curPage = pdfMaxPage; 
     }
 
-    document.getElementById(pdfId + "Pages").src = "/Showcase Site/Documents/Wellness Hub/" + pdfId + curPage + ".jpg"; 
+    document.getElementById(pdfId + "Pages").src = "Documents/Wellness Hub/" + pdfId + curPage + ".jpg"; 
 }
-
 
